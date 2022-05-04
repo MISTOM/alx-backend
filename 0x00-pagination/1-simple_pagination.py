9dde0c6190d53676afe802f20eaeb851be7086d6
MISTOM <kigardetom2001@gmail.com>
2022-05-04 08:51:16 +0300

    Pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,11 +8,11 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        '''Retrieves the index range from a given page and page size
-        '''
-        start = (page - 1) * page_size
-        end = start + page_size
-        return (start, end)
+    '''Retrieves the index range from a given page and page size
+    '''
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
