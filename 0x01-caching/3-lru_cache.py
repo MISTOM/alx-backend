fe5847dc68e741433f58d1360906743f2e861f5f
MISTOM <kigardetom2001@gmail.com>
2022-05-06 10:58:52 +0300

    Least Recently Used caching Policy

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Least Recently Used caching Policy
+'''
+
+from collections import OrderedDict
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
+    '''
+    def __init__(self) -> None:
+        '''Initializes the Class
+        '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''Adds item to the Cache
+        '''
+        if key is None or item is None:
+            return
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(True)
+            print('DISCARD:', first_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        '''Retrieves an Item by key
+        '''
+        self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
